e248876a363c32344768d130914e6086b9e8abe7
John <elementtest8@gmail.com>
2023-12-04 14:22:33 +0800

    3333

---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,9 @@
+-- rename both open and close tags in html i.e. <head> </head>
+return {
+  "windwp/nvim-ts-autotag",
+  lazy = false,
+  config = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+}

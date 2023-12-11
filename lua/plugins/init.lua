4eb7ea8e9d796443489bc44e2c11e189b10d2dd6
John <elementtest8@gmail.com>
2023-12-11 22:21:02 +0800

    999

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,6 @@
+--live reload of lsp settings
+--standard plugins.. closely tied to lazy plugin manager
+--dont warrant their own files 
 return {
 	{"folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,3 +1,5 @@
+--displays popup with possible key bindings of command
+--you started typing
 return {
 	"folke/which-key.nvim",
 }

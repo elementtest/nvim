67b2bc57b73431a70229148e6da2fa703c9c27b5
John <elementtest8@gmail.com>
2023-12-17 21:14:13 +0800

    12776653990224

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,6 @@
 -- used as instructed in lazy nvim documentation to enable this folder
 -- to be a working plugins folder with each plugin as a seperate file
+-- any file in ~/.config/nvim/lua/plugins/ will be merged
 return {
 	{"folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",

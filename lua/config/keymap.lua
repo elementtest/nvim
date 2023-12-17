fc6e7578929e535eb39b119e961b6991836ba366
John <elementtest8@gmail.com>
2023-12-17 21:24:12 +0800

    846274967272

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -221,6 +221,7 @@ vim.opt.shortmess:append({ I = true })
 -- suppresses intro screen message 
 -- use :intro to open 
 
--- quick write quit exit vim
-keymap.set('n', '<leader>q', ':wq<CR>')
+-- Define your custom leader key
+vim.g.mapleader = " "
+
 

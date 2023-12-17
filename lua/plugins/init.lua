e86627e41f00909916f46c09e93e97353b3c48c0
John <elementtest8@gmail.com>
2023-12-17 21:12:42 +0800

    476902455

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,5 @@
---live reload of lsp settings
---standard plugins.. closely tied to lazy plugin manager
---dont warrant their own files 
+-- used as instructed in lazy nvim documentation to enable this folder
+-- to be a working plugins folder with each plugin as a seperate file
 return {
 	{"folke/neoconf.nvim", cmd = "Neoconf" },
 	"folke/neodev.nvim",

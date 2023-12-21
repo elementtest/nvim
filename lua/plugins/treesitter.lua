5f1d5f18b2e148e1227f11b65a6a0965608e0bbd
John <elementtest8@gmail.com>
2023-12-21 14:12:35 +0800

    74893572489572493

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -41,7 +41,7 @@ return {
         incremental_selection = {
           enable = true,
           keymaps = {
-            init_selection = "<C-space>",
+            init_selection = "<C-x>",
             node_incremental = "<C-space>",
             scope_incremental = false,
             node_decremental = "<bs>",

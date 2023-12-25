ef23a95ae25dbb5cf01bd6fdad4d74150a5fbe16
John <elementtest8@gmail.com>
2023-12-25 13:53:56 +0800

    1111

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,18 @@
 --MASON:  package manager install manager LSP servers/linters/formatters
-return     {
-        'williamboman/mason.nvim',
-        lazy = false,
-        config = function()
-            require('mason').setup({
-              ui = {
-                icons = {
-                  package_installed = "✓",
-                  package_pending = "➜",
-                  package_uninstalled = "✗"
-                }
-              }
-            })
-        end
+ return     {
+         'williamboman/mason.nvim',
+         lazy = false,
+         config = function()
+             require('mason').setup({
+               ui = {
+                 icons = {
+                   package_installed = "✓",
+                   package_pending = "➜",
+                   package_uninstalled = "✗"
+                 }
+               }
+             })
+         end
 
-}
+ }
+

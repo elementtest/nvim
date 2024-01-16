fcb3eb350a2fe99c84242f8580c9e4a0259f8a46
John <elementtest8@gmail.com>
2024-01-16 22:49:45 +0800

    65784009456054094574638

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,9 +1,26 @@
+--live server to see web page changes live run :LiveServer on webpage
 return {
-    --live server to see web page changes live run :LiveServer on webpage
     --see keymap for more info
     'aurum77/live-server.nvim',
-    config = function()
+    build = function()
         require"live_server.util".install()
     end,
-    cmd = { "LiveServer", "LiverServerStart", "LiveServerStop"}
-}
+
+    -- config = function()
+        --     local status_ok, live_server = pcall(require, "live_server")
+        --     if not status_ok then
+        --         print("ok")
+        --         return
+        --     end
+        --
+        --
+        --     live_server.setup({
+            --         port = 7567,
+            --         browser_command = "/usr/bin/google-chrome", -- Command or executable path
+            --         quiet = false,
+            --         no_css_inject = true,
+            --         install_path = os.getenv("HOME") .. "/live/",
+            --     })
+            -- end,
+            cmd = { "LiveServer", "LiverServerStart", "LiveServerStop"}
+        }

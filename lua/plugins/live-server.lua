ea2c623b8b2efac1a25b52fc79bb53b77deef64a
elementtest <elementtest8@gmail.com>
2024-06-02 12:55:29 +0800

    119752217828256

--- lua/plugins/live-server.lua
+++
@@ -1,9 +0,0 @@
---live server to see web page changes live run :LiveServer on webpage
-return {
-    --see keymap for more info
-    'aurum77/live-server.nvim',
-    build = function()
-        require"live_server.util".install()
-    end,
-            cmd = { "LiveServer", "LiverServerStart", "LiveServerStop"}
-        }

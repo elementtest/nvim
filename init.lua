1ae82820a5552c4cc3f781de4cdb6bf57a7ea38f
elementtest <elementtest8@gmail.com>
2024-06-05 12:05:46 +0800

    226921842032118

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 require('config')
--- since the file in the config directory is init.lua
--- you dont need anything else in the path
--- path is ~/.config/nvim/lua/config/init.lua
-
+require('oil').setup()
 
 vim.o.clipboard = "unnamedplus"
 

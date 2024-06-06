81ca39d63e36020a2364681eff0e413090c3e0c6
elementtest <elementtest8@gmail.com>
2024-06-06 10:37:18 +0800

    FIXED OIL.NVIM 15577101574061

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,11 @@
 return {
   'stevearc/oil.nvim',
   opts = {},
+  lazy = false,
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+      require('oil').setup({})
+  end
 
 }

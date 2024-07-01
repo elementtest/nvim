88b0b67febb6fffd7e93a4480f39e3a498ac5787
elementtest <elementtest8@gmail.com>
2024-07-01 22:43:01 +0800

    313722388014501

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -319,16 +319,3 @@ keymap.set('n', '<leader>l', '<Cmd>LspInfo<CR>')
 -- do something
 -- end)
 
--- CSS KEYMAP to expand star asterisk
-    vim.keymap.set('i', '*<leader>', function()
-        -- Insert the characters * { }
-        vim.api.nvim_put({ '* { }' }, 'c', true, true)
-
-        -- Move the cursor back inside the braces
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] - 2 })
-
-        -- Simulate pressing the Enter key
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, false, true), 'n', true)
-    end, { noremap = true, silent = true })
-

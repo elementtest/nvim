a40ee3471e4eec2d3c071cda25ea927a4a81b855
elementtest <elementtest8@gmail.com>
2025-12-08 20:58:43 +0800

    removed illuminate-vim (not useful for me now)

--- lua/plugins/illuminate-vim.lua
+++
@@ -1,9 +0,0 @@
--- illuminates all instances of a keyword in doc
--- use alt + n or alt + p to navigate through instances
-return {
-  "RRethy/vim-illuminate",
-  lazy = false,
-  config = function()
-    require('illuminate').configure({})
-  end
-}

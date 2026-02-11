35d0e88563310ba032a8b66da07aca6c7fa214f7
elementtest <elementtest8@gmail.com>
2026-02-11 23:58:53 +0800

    k

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,25 @@
-return     {
-        'nvim-treesitter/nvim-treesitter',
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" }, 
-        -- TS MEANS TREE SITTER
-        opts = {
-                ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
-                -- Install parsers synchronously (only applied to `ensure_installed`)
-                sync_install = false,
-                -- Automatically install missing parsers when entering buffer
-                auto_install = false,
-                highlight = {
-                    enable = true,
+return {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    lazy = false,
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
+            sync_install = false,
+            auto_install = false,
+            highlight = {
+                enable = true,          -- enable Treesitter highlighting
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
                 },
+            },
+        }
+    end
+}
 
-               incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "gnn",
-                        node_incremental = "grn",
-                        scope_incremental = "grc",
-                        node_decremental = "grm",
-                    },
-                },
-
-            }
-    }

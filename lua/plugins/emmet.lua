e903bfec8cd0b324597887c712cebe5e10086d8e
elementtest <elementtest8@gmail.com>
2026-03-16 16:35:42 +0800

    remove trailing space

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -3,11 +3,11 @@ return {
   -- Using 'keys' ensures the mapping exists on startup 
   -- and triggers the plugin to load only when pressed.
   keys = {
-    { 
-      "<leader>we", 
-      function() require('nvim-emmet').wrap_with_abbreviation() end, 
-      mode = { "n", "v" }, 
-      desc = "Emmet Wrap" 
+    {
+      "<leader>we",
+      function() require('nvim-emmet').wrap_with_abbreviation() end,
+      mode = { "n", "v" },
+      desc = "Emmet Wrap"
     },
   },
 }

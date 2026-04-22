9dd52e41d93c646b2f1dbd0919a34df7bc13fe09
elementtest <elementtest8@gmail.com>
2026-04-22 15:21:16 +0800

    remove luasnpe

--- lua/plugins/cmp-blink.lua
+++ lua/plugins/cmp-blink.lua
@@ -1,6 +1,6 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets", "L3MON4D3/LuaSnip" },
+	dependencies = { "rafamadriz/friendly-snippets" },
 
 	version = "1.*",
 	-- change color when using signature hep

9726de4dd4630198fd10c41de61b612916ce27c0
elementtest <elementtest8@gmail.com>
2026-04-22 17:02:52 +0800

    add luasnip

--- lua/plugins/cmp-blink.lua
+++ lua/plugins/cmp-blink.lua
@@ -1,6 +1,6 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = { "rafamadriz/friendly-snippets", 'L3MON4D3/LuaSnip', version = 'v2.*' },
 
 	version = "1.*",
 	-- change color when using signature hep

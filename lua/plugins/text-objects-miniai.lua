d4424af646056b482435febbf02d61e70f66187d
elementtest <elementtest8@gmail.com>
2026-05-02 16:39:10 +0800

    !!!! ADD CUSTOM CSS QUERIES FILE + ADD mini.ai TEXT OBJECTS SHORTcUT!!!!!!

--- lua/plugins/text-objects-miniai.lua
+++ lua/plugins/text-objects-miniai.lua
@@ -1,33 +1,37 @@
 return {
-    'echasnovski/mini.ai',
-    version = false,
-    event = "VeryLazy",
-    config = function()
-        local ai = require('mini.ai')
-        ai.setup({
-            n_lines = 100,
-            custom_textobjects = {
-                f = ai.gen_spec.treesitter({
-                    a = '@function.outer',
-                    i = '@function.inner',
-                }),
-                ['='] = ai.gen_spec.treesitter({
-                    a = '@assignment.outer',
-                    i = '@assignment.rhs',
-                }),
-                   ['L'] = ai.gen_spec.treesitter({
-                    a = '@assignment.outer',
-                    i = '@assignment.lhs',
-                }),
-                a = ai.gen_spec.treesitter({
-                    a = '@parameter.outer',
-                    i = '@parameter.inner',
-                }),
-                c = ai.gen_spec.treesitter({
-                    a = '@conditional.outer',
-                    i = '@conditional.inner',
-                }),
-            },
-        })
-    end
+	"echasnovski/mini.ai",
+	version = false,
+	event = "VeryLazy",
+	config = function()
+		local ai = require("mini.ai")
+		ai.setup({
+			n_lines = 100,
+			custom_textobjects = {
+				f = ai.gen_spec.treesitter({
+					a = "@function.outer",
+					i = "@function.inner",
+				}),
+				["="] = ai.gen_spec.treesitter({
+					a = "@assignment.outer",
+					i = "@assignment.rhs",
+				}),
+				["L"] = ai.gen_spec.treesitter({
+					a = "@assignment.outer",
+					i = "@assignment.lhs",
+				}),
+				a = ai.gen_spec.treesitter({
+					a = "@parameter.outer",
+					i = "@parameter.inner",
+				}),
+				c = ai.gen_spec.treesitter({
+					a = "@conditional.outer",
+					i = "@conditional.inner",
+				}),
+				["-"] = ai.gen_spec.treesitter({
+					a = "@value.outer",
+					i = "@value.inner",
+				}),
+			},
+		})
+	end,
 }
---
+++ queries/css/textobjects.scm
@@ -0,0 +1,10 @@
+;; extends
+
+(declaration
+  (integer_value) @value.inner) @value.outer
+
+(declaration
+  (float_value) @value.inner) @value.outer
+
+(declaration
+  (color_value) @value.inner) @value.outer

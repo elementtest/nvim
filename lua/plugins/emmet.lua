04b35bde18eb2b881db30573f0c4fe9900fc81c7
elementtest <elementtest8@gmail.com>
2026-05-13 13:27:54 +0800

    ! EMMET CONTROL + J NOW JUMPS TO NEXT EDITABLE THING

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,16 +1,22 @@
 return {
 	"mattn/emmet-vim",
-  ft = { "html" },
-  init = function()
-    vim.g.user_emmet_leader_key = ','
-    -- vim.g.user_emmet_mode = 'nv'
-  end,
+	ft = { "html" },
+	init = function()
+		vim.g.user_emmet_leader_key = ","
+		-- vim.g.user_emmet_mode = 'nv'
+	end,
 	keys = {
 		{
 			"<leader>we",
 			"<plug>(emmet-expand-abbr)",
 			mode = { "v" },
 			desc = "Emmet Wrap",
+		},
+		{
+			"<C-j>", -- or whatever key you want
+			"<plug>(emmet-move-next)",
+			mode = { "i" },
+			desc = "Emmet next edit point",
 		},
 	},
 }

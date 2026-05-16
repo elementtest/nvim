b8060789ceda3dccab3bb8254b380c8fc0608661
elementtest <elementtest8@gmail.com>
2026-05-16 18:48:05 +0800

    !!! live-server update move link away from github

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,8 +1,8 @@
 -- live server installed to make changes in editor appear instantly in browser
 return {
-        'barrett-ruth/live-server.nvim',
-        build = 'pnpm add -g live-server',
-        cmd = { 'LiveServerStart', 'LiveServerStop' },
-        opts = {},
-
-    }
+	"https://git.barrettruth.com/barrettruth/live-server.nvim",
+	-- 'barrett-ruth/live-server.nvim',
+	build = "pnpm add -g live-server",
+	cmd = { "LiveServerStart", "LiveServerStop" },
+	opts = {},
+}
